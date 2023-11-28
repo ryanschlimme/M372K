2ba474ed3515d29eda4c951f30a7b6b2ecd11c28
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-11-28 09:04:28 -0600

    Homework 7

---
+++ Homework 7/Q2.1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def FourierTransform(w):
+    return 1j/(2*np.pi*(w+1j))
+
+xvals = np.linspace(-5, 5, 1000)
+
+fig, (ax0, ax1) = plt.subplots(2,1, sharex = True)
+
+ax0.plot(xvals, np.real(FourierTransform(xvals)), color = "b")
+ax1.plot(xvals, np.imag(FourierTransform(xvals)), color = "r")
+
+fig.suptitle("Fourier Transform (F($\omega$))")
+ax0.set_xlabel("$\omega$ (rad/s)")
+ax0.set_ylabel("$\Re$(F($\omega$))")
+ax1.set_xlabel("$\omega$ (rad/s)")
+ax1.set_ylabel("$\Im$(F($\omega$))")
+#plt.show()
+plt.savefig("Q2.1.png")
